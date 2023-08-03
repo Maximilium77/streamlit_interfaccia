38edea32877b1e8963633402d52dd46385d3e008
Maximilium77 <59537776+Maximilium77@users.noreply.github.com>
2023-08-03 16:56:09 +0200

    Update app_text.py

--- app_text.py
+++ app_text.py
@@ -10,15 +10,11 @@ df = pd.DataFrame({"DATA": data, "VALORE": range(len(data))})
 st.title("Seleziona un intervallo temporale")
 
 # Interfaccia per selezionare l'intervallo
-start_date_str = st.date_input("Seleziona la data di inizio", df['DATA'].min())
-end_date_str = st.date_input("Seleziona la data di fine", df['DATA'].max())
-
-# Conversione delle date in oggetti datetime
-start_date = start_date_str
-end_date = end_date_str
+start_date = st.date_input("Seleziona la data di inizio", df['DATA'].min())
+end_date = st.date_input("Seleziona la data di fine", df['DATA'].max())
 
 # Filtraggio dei dati in base all'intervallo selezionato
-filtered_data = df[(df['DATA'] >= start_date) & (df['DATA'] <= end_date)]
+filtered_data = df[(df['DATA'] >= pd.to_datetime(start_date)) & (df['DATA'] <= pd.to_datetime(end_date))]
 
 # Visualizzazione dei dati filtrati
 st.write("Dati nell'intervallo selezionato:")
