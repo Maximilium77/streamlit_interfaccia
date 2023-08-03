115ba51270198eac300d190b5c5ce0e902c5ee7a
Maximilium77 <59537776+Maximilium77@users.noreply.github.com>
2023-08-03 16:57:52 +0200

    Update app_text.py

--- app_text.py
+++ app_text.py
@@ -2,20 +2,23 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 
-# Creazione di dati di esempio
-data = pd.date_range(start="2023-01-01", end="2023-12-31", freq="D")
-df = pd.DataFrame({"DATA": data, "VALORE": range(len(data))})
-
 # Titolo dell'app
-st.title("Seleziona un intervallo temporale")
+st.title("App con selezione intervallo temporale e upload di un file Excel")
 
-# Interfaccia per selezionare l'intervallo
-start_date = st.date_input("Seleziona la data di inizio", df['DATA'].min())
-end_date = st.date_input("Seleziona la data di fine", df['DATA'].max())
+# Upload del file Excel
+uploaded_file = st.file_uploader("Carica un file Excel", type=["xlsx"])
 
-# Filtraggio dei dati in base all'intervallo selezionato
-filtered_data = df[(df['DATA'] >= pd.to_datetime(start_date)) & (df['DATA'] <= pd.to_datetime(end_date))]
+if uploaded_file is not None:
+    # Leggi il file Excel e crea il DataFrame
+    df = pd.read_excel(uploaded_file)
 
-# Visualizzazione dei dati filtrati
-st.write("Dati nell'intervallo selezionato:")
-st.dataframe(filtered_data)
+    # Interfaccia per selezionare l'intervallo
+    start_date = st.date_input("Seleziona la data di inizio", df['DATA'].min())
+    end_date = st.date_input("Seleziona la data di fine", df['DATA'].max())
+
+    # Filtraggio dei dati in base all'intervallo selezionato
+    filtered_data = df[(df['DATA'] >= pd.to_datetime(start_date)) & (df['DATA'] <= pd.to_datetime(end_date))]
+
+    # Visualizzazione dei dati filtrati
+    st.write("Dati nell'intervallo selezionato:")
+    st.dataframe(filtered_data)
