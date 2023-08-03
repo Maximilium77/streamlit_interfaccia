e6a5797f13c374a5ede1d1416a32b9b9c90fe0f6
Maximilium77 <59537776+Maximilium77@users.noreply.github.com>
2023-08-03 15:51:48 +0200

    Update app_text.py

--- app_text.py
+++ app_text.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime, timedelta
+
+# Creazione di dati di esempio
+data = pd.date_range(start="2023-01-01", end="2023-12-31", freq="D")
+df = pd.DataFrame({"DATA": data, "VALORE": range(len(data))})
+
+# Titolo dell'app
+st.title("Seleziona un intervallo temporale")
+
+# Interfaccia per selezionare l'intervallo
+start_date = st.date_input("Seleziona la data di inizio", df['DATA'].min())
+end_date = st.date_input("Seleziona la data di fine", df['DATA'].max())
+
+# Filtraggio dei dati in base all'intervallo selezionato
+filtered_data = df[(df['DATA'] >= start_date) & (df['DATA'] <= end_date)]
+
+# Visualizzazione dei dati filtrati
+st.write("Dati nell'intervallo selezionato:")
+st.dataframe(filtered_data)
 
