d7a77c6bcafeb3d3b6569da1c0193d41313f0d26
Maximilium77 <59537776+Maximilium77@users.noreply.github.com>
2023-08-03 15:55:54 +0200

    Update app_text.py

--- app_text.py
+++ app_text.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime
 
 # Creazione di dati di esempio
 data = pd.date_range(start="2023-01-01", end="2023-12-31", freq="D")
@@ -10,12 +10,12 @@ df = pd.DataFrame({"DATA": data, "VALORE": range(len(data))})
 st.title("Seleziona un intervallo temporale")
 
 # Interfaccia per selezionare l'intervallo
-start_date = st.date_input("Seleziona la data di inizio", df['DATA'].min())
-end_date = st.date_input("Seleziona la data di fine", df['DATA'].max())
+start_date_str = st.date_input("Seleziona la data di inizio", df['DATA'].min())
+end_date_str = st.date_input("Seleziona la data di fine", df['DATA'].max())
 
 # Conversione delle date in formato 'DD/MM/YYYY' a oggetti datetime
-start_date = datetime.strptime(start_date_str.strftime("%d/%m/%Y"), "%d/%m/%Y")
-end_date = datetime.strptime(end_date_str.strftime("%d/%m/%Y"), "%d/%m/%Y")
+start_date = start_date_str.date() if isinstance(start_date_str, datetime) else datetime.strptime(start_date_str, "%Y-%m-%d")
+end_date = end_date_str.date() if isinstance(end_date_str, datetime) else datetime.strptime(end_date_str, "%Y-%m-%d")
 
 # Filtraggio dei dati in base all'intervallo selezionato
 filtered_data = df[(df['DATA'] >= start_date) & (df['DATA'] <= end_date)]
@@ -23,4 +23,3 @@ filtered_data = df[(df['DATA'] >= start_date) & (df['DATA'] <= end_date)]
 # Visualizzazione dei dati filtrati
 st.write("Dati nell'intervallo selezionato:")
 st.dataframe(filtered_data)
-
